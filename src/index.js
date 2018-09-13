df430af1c1e3891b68266a7cd3c151c628a91a5b
A.Seregin <a.siarohin@gmail.com>
2018-09-14 00:51:08 +0300

    Math equation -theory

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  // a*x^2 + b*x + c = 0
+  // 
+  // d = b^2 - 4a*c
+  //
+  // if d > 0 {                            // +++ 2 *integer* solutions
+  //	x1 = (-b + sqrt d) / 2 * a
+  //    x2 = (-b - sqrt d) / 2 * a
+  // 
+  //  ********************************************************************
+  //
+  // if d = 0                             // --- 1 *integer* solutions
+  //    x = -b / 2 * a
+  //
+  // id d < 0                             // --- NaN
+  //    
+  //
+  // !! *Each* equality has exact 2 *integer* solutions. Return those numbers as ordered array.
+  //
+  // 'll do it at 15th, Sat
+  
 }
